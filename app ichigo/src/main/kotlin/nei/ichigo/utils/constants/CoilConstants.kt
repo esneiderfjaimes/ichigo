801f20fa9365edeac1f0a40681554aff79790c13
Esneider Jaimes <62038623+esneiderfjaimes@users.noreply.github.com>
2025-07-02 23:52:44 -0500

    Refactor: Delete `app ichigo` module
    This commit removes the `app ichigo` module and all its associated files.
    The functionality previously contained within this module has been moved or is no longer required.

--- app ichigo/src/main/kotlin/nei/ichigo/utils/constants/CoilConstants.kt
+++
@@ -1,9 +0,0 @@
-package nei.ichigo.utils.constants
-
-object CoilConstants {
-
-    fun champion(championImage: String): String {
-        return "https://opgg-static.akamaized.net/meta/images/lol/champion/$championImage"
-    }
-
-}
